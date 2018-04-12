d41f93d97edd7e299ebb51c6952cf0874300e8fe
Patrick Ferreira <paatrickferreira@gmail.com>
2018-04-12 14:11:01 -0300

    Refactor structure

---
+++ src/selectors.js
@@ -0,0 +1,14 @@
+const cars = () => '#main-ad-list .item';
+
+const carTitle = () => '.OLXad-list-title';
+
+const carDetails = () => '.detail-specific';
+
+const carPrice = () => '.OLXad-list-price';
+
+module.exports = {
+  cars,
+  carTitle,
+  carDetails,
+  carPrice,
+};
